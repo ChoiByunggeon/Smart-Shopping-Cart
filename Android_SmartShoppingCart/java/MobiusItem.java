0b8cc5a94865c00502723efb0167efab792b51b1
ChoiByunggeon <90560302+ChoiByunggeon@users.noreply.github.com>
2021-11-13 03:09:08 +0900

    Add files via upload

---
+++ Android_SmartShoppingCart/java/MobiusItem.java
@@ -0,0 +1,27 @@
+package com.example.shoppingcart;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class MobiusItem implements Serializable {
+    String pi;
+    String ri;
+    int ty;
+    String ct;
+    String rn;
+    String lt;
+    String con;
+    String et;
+    ArrayList<String> lbl = new ArrayList<>();
+    String api;
+    String aei;
+    ArrayList<String> poa = new ArrayList<>();
+    boolean rr;
+
+}
+
+
+
+
