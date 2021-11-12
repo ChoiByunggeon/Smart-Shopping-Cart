57941bd4c1c2b587241eb76cbf369095ab4a5ac5
ChoiByunggeon <90560302+ChoiByunggeon@users.noreply.github.com>
2021-11-12 18:23:30 +0900

    Delete shoppingcart directory

--- shoppingcart/MobiusService.java
+++
@@ -1,9 +0,0 @@
-package com.example.shoppingcart;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface MobiusService {
-    @GET("/Mobius/jws/la/")
-    Call<MobiusResult> info();
-}
